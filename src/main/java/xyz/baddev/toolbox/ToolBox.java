835da0f340bd29491a849c04f4466ac55ba696cf
Baddeveloper <admin@baddeveloper.xyz>
2018-08-02 17:19:53 +0200

    Init commit

---
+++ src/main/java/xyz/baddev/toolbox/ToolBox.java
@@ -0,0 +1,16 @@
+package xyz.baddev.toolbox;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class ToolBox extends PluginBase {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
