d1602fec5c725e02c55d830fcfd309b2deb2393e
Baddeveloper <admin@baddeveloper.xyz>
2018-08-02 21:07:13 +0200

    Added all managers

--- src/main/java/xyz/baddev/toolbox/ToolBox.java
+++ src/main/java/xyz/baddev/toolbox/ToolBox.java
@@ -3,25 +3,40 @@ package xyz.baddev.toolbox;
 import cn.nukkit.event.Listener;
 import cn.nukkit.plugin.PluginBase;
 import xyz.baddev.toolbox.manager.managers.player.MOTDManager;
+import xyz.baddev.toolbox.manager.managers.player.MessagesManager;
 import xyz.baddev.toolbox.manager.managers.utils.FileManager;
+import xyz.baddev.toolbox.manager.managers.utils.PlaceholderManager;
 
 public class ToolBox extends PluginBase {
 
+    private static ToolBox instance;
+
     private FileManager fileManager;
+    private PlaceholderManager placeholderManager;
+    private MessagesManager messagesManager;
     private MOTDManager motdManager;
 
     @Override
     public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
         fileManager = new FileManager();
+        placeholderManager = new PlaceholderManager();
+        messagesManager = new MessagesManager();
         motdManager = new MOTDManager();
 
         fileManager.onEnable(this);
+        placeholderManager.onEnable(this);
+        messagesManager.onEnable(this);
         motdManager.onEnable(this);
     }
 
     @Override
     public void onDisable() {
         motdManager.onDisable(this);
+        messagesManager.onDisable(this);
+        placeholderManager.onDisable(this);
         fileManager.onDisable(this);
     }
 
@@ -29,7 +44,19 @@ public class ToolBox extends PluginBase {
         getServer().getPluginManager().registerEvents(listener, this);
     }
 
+    public static ToolBox getInstance() {
+        return instance;
+    }
+
     public FileManager getFileManager() {
         return fileManager;
+    }
+
+    public PlaceholderManager getPlaceholderManager() {
+        return placeholderManager;
+    }
+
+    public MOTDManager getMotdManager() {
+        return motdManager;
     }
 }
--- src/main/java/xyz/baddev/toolbox/utils/TextUtils.java
+++ src/main/java/xyz/baddev/toolbox/utils/TextUtils.java
@@ -1,10 +1,22 @@
 package xyz.baddev.toolbox.utils;
 
+import cn.nukkit.Player;
 import cn.nukkit.utils.TextFormat;
+import xyz.baddev.toolbox.ToolBox;
 
 public class TextUtils {
 
     public static String colorizeText(String text) {
         return TextFormat.colorize('&', text);
+    }
+
+    public static String placeholderify(String text, Player player) {
+        final ToolBox instance = ToolBox.getInstance();
+        return instance.getPlaceholderManager().isAvailable() ? instance.getPlaceholderManager().getPlaceholderAPI().translateString(text, player) : text;
+    }
+
+    public static String placeholderify(String text) {
+        final ToolBox instance = ToolBox.getInstance();
+        return instance.getPlaceholderManager().isAvailable() ? instance.getPlaceholderManager().getPlaceholderAPI().translateString(text) : text;
     }
 }
